3f8fa435574150b2e685bcddca3f63e0ebd76283
harshvardhan Singh Shekhawat <harshvardhan.shekhawat93@gmail.com>
2026-01-26 14:47:00 +0530

    Added a new project for book-service to handle CRUD for books in the library
    Signed-off-by: Harshvardhan Singh Shekhawat  <harshvardhan.shekhawat93@gmail.com>

---
+++ book-service/src/main/java/com/health/spry/LoginService1Application.java
@@ -0,0 +1,13 @@
+package com.health.spry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginService1Application.class, args);
+	}
+
+}

7a4c785f8f955f38f3b2cea969da45d9219bdd01
harshvardhan Singh Shekhawat <harshvardhan.shekhawat93@gmail.com>
2026-01-26 13:29:59 +0530

    Added new Spring boot service to handle login for LMS user . Added properties.yaml to link Eureka and addwd JWT props and MySQl props Signed-off-by: Harshvardhan Singh Shekhawat  <harshvardhan.shekhawat93@gmail.com>

---
+++ login-service/src/test/java/com/health/spry/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.spry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

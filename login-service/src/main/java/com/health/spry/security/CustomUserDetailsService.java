83a1f8fb24c86b57f45e8a0badc740e473b80b55
harshvardhan Singh Shekhawat <harshvardhan.shekhawat93@gmail.com>
2026-01-26 13:41:00 +0530

    Added Sprong Security's custom Userdtail service to handle username/PAss Auth
    Signed-off-by: Harshvardhan Singh Shekhawat  <harshvardhan.shekhawat93@gmail.com>

---
+++ login-service/src/main/java/com/health/spry/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.health.spry.security;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.health.spry.model.User;
+import com.health.spry.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getActive(),
+                true,
+                true,
+                true,
+                new ArrayList<>()
+        );
+    }
+}

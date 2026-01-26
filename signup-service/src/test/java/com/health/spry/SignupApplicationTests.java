51e3876f8014172a6a6aa0c21c34f327fdec90dc
harshvardhan Singh Shekhawat <harshvardhan.shekhawat93@gmail.com>
2026-01-26 13:10:43 +0530

    Updaetd the main calss name fir signup service
    Signed-off-by: Harshvardhan Singh Shekhawat  <harshvardhan.shekhawat93@gmail.com>

--- signup-service/src/test/java/com/health/spry/SignupApplicationTests.java
+++ signup-service/src/test/java/com/health/spry/SignupApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGateway1ApplicationTests {
+class SignupApplicationTests {
 
 	@Test
 	void contextLoads() {
